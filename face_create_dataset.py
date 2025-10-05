6734fce02190243dde4c5ac2ec3be6a9ff6b5ceb
Fachreza28 <162418737+Fachreza28@users.noreply.github.com>
2025-10-05 19:03:29 +0700

    Update face_create_dataset.py
    haarcascade

--- face_create_dataset.py
+++ face_create_dataset.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 # Inisialisasi Cascade Classifier dan Webcam
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 cap = cv2.VideoCapture(1) 
 
 # Path utama untuk dataset
